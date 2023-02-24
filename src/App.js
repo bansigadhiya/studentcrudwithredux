b2cbdcd117dd1fd34537dbe8156d5d16d3c537fb
bansigadhiya <bansi672004@gmail.com>
2023-02-24 19:11:16 +0530

    update student

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import CreateStudent from './Components/Create-student/CreateStudent';
+import EditStudent from './Components/Edit-student/EditStudent';
 import ViewStudent from './Components/View-students-data/ViewStudent';
 
 function App() {
+
+  const [isEdit , setIsEdit] = useState(false);
+
+  const handleEdit = () => {
+    console.log("handleeditcalled");
+    setIsEdit(!isEdit);
+  }
   return (
     <div>
-      <CreateStudent />
-      <ViewStudent />
+      {
+        isEdit ? <EditStudent handleEdit={handleEdit}/> : <CreateStudent />
+      }
+      <ViewStudent handleEdit={handleEdit}/>
     </div>
   );
 }
