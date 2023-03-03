4b9f0ea515af7d5e6b06c36ce4875a6e6a447c02
bansigadhiya <bansi672004@gmail.com>
2023-03-03 18:49:44 +0530

    redux persist

--- src/Store.js
+++ src/Store.js
@@ -1,7 +1,18 @@
 import { createStore } from "redux";
 import Rootreducer from "./Services/Reducers/Rootreducer";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key : "reduxStudentCrud",
+    storage
+}
+
+const persistR = persistReducer(persistConfig,Rootreducer)
 
 const store = createStore(
-    Rootreducer , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    persistR , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+export const persistor = persistStore(store)
 export default store;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import store from './Store';
+import store, { persistor } from './Store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
